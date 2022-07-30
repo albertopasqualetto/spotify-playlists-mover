5a0eb36a38d9c5a1c8b7d9f7176d34ca2d7d5054
Alberto Pasqualetto <39854348+albertopasqualetto@users.noreply.github.com>
2022-07-30 14:28:32 +0200

    Logout refactored

--- __main__.py
+++ __main__.py
@@ -5,12 +5,15 @@ import time
 
 
 def main():
+	spotify_logout()
 	old_account_dl= spotifyplaylistmover.download_from_origin()
-	webbrowser.open('accounts.spotify.com/logout')	# logout from old account
-	time.sleep(3)
+	spotify_logout()
 	spotifyplaylistmover.upload_to_destination(old_account_dl)
 	print("Done!")
 
+def spotify_logout():
+	webbrowser.open('accounts.spotify.com/logout')	# logout from old account
+	time.sleep(3)
 
 if __name__ == '__main__':
 	sys.exit(main())
